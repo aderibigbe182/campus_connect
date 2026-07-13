eda219df9e4932d43e5655def399353a722d632c
aderibigbe182 <aderibigbesamson103@gmail.com>
2026-07-13 16:25:16 +0100

    Enhance ChatHomeScreen layout with app bar and search
    Refactor ChatHomeScreen to include app bar, search bar, and chat list.

--- lib/features/chat/screens/chat_home_screen.dart
+++ lib/features/chat/screens/chat_home_screen.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 
+import '../models/chat_model.dart';
+import '../services/chat_service.dart';
+import '../widgets/chat_app_bar.dart';
+import '../widgets/chat_search_bar.dart';
+import '../widgets/chat_tile.dart';
+
 class ChatHomeScreen extends StatefulWidget {
   const ChatHomeScreen({super.key});
 
@@ -10,10 +16,58 @@ class ChatHomeScreen extends StatefulWidget {
 
 class _ChatHomeScreenState
     extends State<ChatHomeScreen> {
+
+  final ChatService service = ChatService();
+
+  late Future<List<ChatModel>> chats;
+
+  @override
+  void initState() {
+    super.initState();
+    chats = service.getChats();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.white,
+    return Scaffold(
+      appBar: const ChatAppBar(),
+      body: Column(
+        children: [
+          const SizedBox(height: 10),
+
+          const ChatSearchBar(),
+
+          const SizedBox(height: 10),
+
+          Expanded(
+            child: FutureBuilder<List<ChatModel>>(
+              future: chats,
+              builder: (context, snapshot) {
+
+                if (!snapshot.hasData) {
+                  return const Center(
+                    child:
+                        CircularProgressIndicator(),
+                  );
+                }
+
+                final data = snapshot.data!;
+
+                return ListView.builder(
+                  itemCount: data.length,
+                  itemBuilder: (context, index) {
+
+                    return ChatTile(
+                      chat: data[index],
+                      onTap: () {},
+                    );
+                  },
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
